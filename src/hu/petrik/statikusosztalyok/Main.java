b48c6aca195c48049288cfe3358a2d8011d2c5de
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-06 09:03:39 +0200

    Veletlen class fel 2-ig megvan

--- src/hu/petrik/statikusosztalyok/Main.java
+++ src/hu/petrik/statikusosztalyok/Main.java
@@ -13,5 +13,13 @@ public class Main {
         for (int i = 0; i < 100; i++) {
             System.out.println(Veletlen.velEgesz(5, 10));;
         }
+        for (int i = 0; i < 50; i++) {
+            System.out.println(Veletlen.Velkara((char) 65, (char) 90));
+            //System.out.println(Veletlen.Velkara('A','B'));
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Veletlen.velTeljesNev(true));
+        }
     }
 }
--- src/hu/petrik/statikusosztalyok/Veletlen.java
+++ src/hu/petrik/statikusosztalyok/Veletlen.java
@@ -33,4 +33,39 @@ public final class Veletlen {
     public static int velEgesz(int min, int max){
         return rnd.nextInt(max - min + 1) + min;
     }
+
+    public static char Velkara(char min, char max){
+        return (char) velEgesz(min, max);
+    }
+
+    public static String velVezetek(){
+        return vezNevek.get(rnd.nextInt(vezNevek.size()));
+    }
+
+    /**
+     * Véletlen magyar keresztnév generálása
+     * @param nem A generált név nem. Férfi esetén true, Nő esetén false.
+     * @return
+     */
+    public static String velKeresztNev(boolean nem){
+        String keresztNev = null;
+        if (nem){
+            keresztNev = velFerfiKeresztNev();
+        }else {
+            keresztNev = velNoiKeresztNev();
+        }
+        return keresztNev;
+    }
+
+    private static String velFerfiKeresztNev(){
+        return ferfiKerNevek.get(rnd.nextInt(ferfiKerNevek.size()));
+    }
+
+    private static String velNoiKeresztNev(){
+        return noiKerNevek.get(rnd.nextInt(ferfiKerNevek.size()));
+    }
+
+    public static String velTeljesNev(boolean nem){
+        return velVezetek() + " " + velKeresztNev(nem);
+    }
 }
